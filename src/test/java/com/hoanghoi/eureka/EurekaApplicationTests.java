71895d5bd596e76cc0af6ee68512648dc9fe8ce9
dangtienhoang37 <dangtienhoang37@gmail.com>
2025-01-05 17:51:31 +0700

    first commit

---
+++ src/test/java/com/hoanghoi/eureka/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hoanghoi.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
